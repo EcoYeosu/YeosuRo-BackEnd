b630fffb473b424c29093b562f4b77bd05635b92
Donga1125 <ehddk0133@naver.com>
2024-07-05 17:21:59 +0900

    ✨feat: 게시글 작성 시 포인트 지급 기능 작업 #23

--- src/main/java/greenjangtanji/yeosuro/user/dto/UserResponseDto.java
+++ src/main/java/greenjangtanji/yeosuro/user/dto/UserResponseDto.java
@@ -1,26 +1,54 @@
 package greenjangtanji.yeosuro.user.dto;
 import greenjangtanji.yeosuro.user.entity.User;
+import greenjangtanji.yeosuro.user.repostory.UserRepository;
+import greenjangtanji.yeosuro.user.service.UserService;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponseDto {
-    private Long id;
-    private String email;
-    private String password;
-    private String nickname;
-    private Boolean agree;
-    private String profileImageUrl;
-    private Enum role;
 
-    public UserResponseDto (User user){
-        this.id = user.getId();
-        this.email = user.getEmail();
-        this.password = user.getPassword();
-        this.nickname = user.getNickname();
-        this.agree = user.getAgree();
-        this.profileImageUrl = user.getProfileImageUrl();
-        this.role = user.getRole();
+    //private static UserService userService;
+
+    @Getter
+    @NoArgsConstructor
+    public static class DetailUserInfo {
+        private Long id;
+        private String email;
+        private String password;
+        private String nickname;
+        private Boolean agree;
+        private String profileImageUrl;
+        private Enum role;
+
+        public DetailUserInfo (User user){
+            this.id = user.getId();
+            this.email = user.getEmail();
+            this.password = user.getPassword();
+            this.nickname = user.getNickname();
+            this.agree = user.getAgree();
+            this.profileImageUrl = user.getProfileImageUrl();
+            this.role = user.getRole();
+        }
+    }
+
+
+    @Getter
+    @NoArgsConstructor
+    public static class BriefUserInfo {
+        private Long id;
+        private String nickname;
+        private String profileImageUrl;
+        private int totalPoint;
+        private String tier;
+
+        public BriefUserInfo (User user){
+            this.id = user.getId();
+            this.nickname = user.getNickname();
+            this.profileImageUrl = user.getProfileImageUrl();
+            this.totalPoint = user.getTotalPoint();
+            //this.tier =
+        }
     }
 }
